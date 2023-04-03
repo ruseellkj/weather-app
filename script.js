cbf98b180fbc8374b9bc5721244c8e7f31a0f1c4
Rushil Jariwala <rkjjariwala@gmail.com>
2023-04-04 00:35:43 +0530

    updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '88a6afe50emsh7b1fc7e06b88d58p1f613ejsnbbb6cba00d5f',
+        // 'X-RapidAPI-Key': '',
         'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
     }
 };
